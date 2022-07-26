4ede6c38a68ce344b3af59489d08e0153334b82f
Tomek Rychtyk <tomek@rychtyk.pl>
2022-07-27 01:05:05 +0200

    Executing user's scripts in an iframe

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
   const ref = useRef<any>();
+  const iframe = useRef<any>();
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -36,8 +37,23 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+        <script>
+          window.addEventListener("message", (event) => {
+            eval(event.data);
+          }, false)
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <div>
@@ -49,6 +65,12 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe
+        ref={iframe}
+        title="codeEditor"
+        srcDoc={html}
+        sandbox="allow-scripts"
+      />
     </div>
   );
 };
