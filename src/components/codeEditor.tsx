b2c4b3a1e1a42fea7da7e12fd3e8cfcc0d982cdc
Tomek Rychtyk <tomek@rychtyk.pl>
2022-08-05 02:27:23 +0200

    Automatic formatting

--- src/components/codeEditor.tsx
+++ src/components/codeEditor.tsx
@@ -1,4 +1,10 @@
+import { useRef } from "react";
+import "bulmaswatch/superhero/bulmaswatch.min.css";
 import MonacoEditor from "@monaco-editor/react";
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
+import "./codeEditor.css";
 
 interface CodeEditorProps {
   initialValue: string;
@@ -9,28 +15,62 @@ const CodeEditor: React.FC<CodeEditorProps> = ({
   initialValue,
   onChange,
 }: CodeEditorProps) => {
+  const editorRef = useRef<any>(null);
+  const valueRef = useRef<any>();
   const handleEditorChange = (value: string | undefined) => {
+    valueRef.current = value;
     onChange(value);
   };
 
+  const handleEditorDidMount = (
+    editor: monaco.editor.IStandaloneCodeEditor
+  ): void => {
+    editorRef.current = editor;
+  };
+
+  const onFormatClick = () => {
+    const unformatted = valueRef.current;
+    const formatted = prettier
+      .format(unformatted, {
+        parser: "babel",
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, "");
+
+    editorRef.current.setValue(formatted);
+  };
+
   return (
-    <MonacoEditor
-      onChange={handleEditorChange}
-      value={initialValue}
-      language="javascript"
-      height="500px"
-      theme="vs-dark"
-      options={{
-        wordWrap: "on",
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-    />
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
+      <MonacoEditor
+        onChange={handleEditorChange}
+        onMount={handleEditorDidMount}
+        value={initialValue}
+        language="javascript"
+        height="500px"
+        theme="vs-dark"
+        options={{
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+          tabSize: 2,
+        }}
+      />
+    </div>
   );
 };
 
